1ca2d1677cd74e17dbb20481b04c20ab9f75abe9
ghost-ts <thushan2004s@gmail.com>
2025-01-31 14:50:40 +0530

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function() {
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/tasks', function () {
+    return view('index', [
+        'tasks' => \App\Models\Task::latest()->get()
+    ]);
+})->name('tasks.index');
+
+Route::view('/tasks/create', 'create')
+    ->name('tasks.create');
+
+Route::get('/tasks/{id}', function ($id) {
+    return view('show', [
+        'task' => \App\Models\Task::findOrFail($id)
+    ]);
+})->name('tasks.show');
+
+Route::post('/tasks', function(Request $request) {
+    dd($request->all());
+})->name('tasks.store');
+
+// Route::get('/xxx', function() {
+//     return 'Hello World';
+// })->name('hello');
+
+// Route::get('/hallo', function() {
+//     return redirect()->route('hello');
+// });
+
+// Route::get('/greet/{name}', function($name) {
+//     return 'Hello ' . $name;
+// });
+
+Route::fallback(function() {
+    return 'No page found';
+});
